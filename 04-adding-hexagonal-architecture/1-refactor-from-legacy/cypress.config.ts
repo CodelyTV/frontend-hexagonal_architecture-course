6f94a8d12858000e5f22836344f7c020a34aea17
Nuria <soriano.nuria@gmail.com>
2023-03-20 11:55:54 +0100

    feat(04-01): refactor from legacy step 2 add tests

---
+++ 04-adding-hexagonal-architecture/1-refactor-from-legacy/cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    specPattern: "tests/e2e-integration/tests/**/*.spec.{js,jsx,ts,tsx}",
+    screenshotOnRunFailure: false,
+    video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    supportFile: "tests/e2e-integration/support/e2e.ts",
+  },
+});
---
+++ 04-adding-hexagonal-architecture/1-refactor-from-legacy/tests/e2e-integration/tests/home.spec.ts
@@ -0,0 +1,12 @@
+describe("The Home Page", () => {
+  it("creates a course when filling up the form correctly", () => {
+    cy.visit("/");
+    cy.findByLabelText(/Course title/i).type("Awesome Hexagonal Architecture");
+    cy.findByLabelText(/Image URL/i).type("http://placekitten.com/500/400");
+    cy.findByText(/create course/i).click();
+    cy.findByRole("heading", { name: /Course created/i }).should("exist");
+    cy.findByRole("heading", { name: "Awesome Hexagonal Architecture" }).should(
+      "exist"
+    );
+  });
+});
