e93de5421ebe7df46595b0a4e8283d0b14a4f384
Nuria <soriano.nuria@gmail.com>
2023-03-22 15:44:00 +0100

    feat(05-02): object mother step 1

---
+++ 05-one-more-step/2-object-mothers/src/sections/courses/CoursesContext.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { createCourse } from "../../modules/courses/application/create/createCourse";
+import { getAllCourses } from "../../modules/courses/application/get-all/getAllCourses";
+import { Course } from "../../modules/courses/domain/Course";
+import { CourseRepository } from "../../modules/courses/domain/CourseRepository";
+
+export interface ContextState {
+	courses: Course[];
+	createCourse: (course: { title: string; imageUrl: string }) => void;
+}
+
+export const CoursesContext = React.createContext({} as ContextState);
+
+export const CoursesContextProvider = ({
+	children,
+	repository,
+}: React.PropsWithChildren<{ repository: CourseRepository }>) => {
+	const [courses, setCourses] = useState<Course[]>([]);
+
+	function create({ title, imageUrl }: { title: string; imageUrl: string }) {
+		const id = (uuidv4 as () => string)(); // TODO: check uuid types
+
+		createCourse(repository, { id, title, imageUrl });
+		getCourses();
+	}
+
+	function getCourses() {
+		const courses = getAllCourses(repository);
+		setCourses(courses);
+	}
+
+	useEffect(() => {
+		getCourses();
+	}, []);
+
+	return (
+		<CoursesContext.Provider value={{ courses, createCourse: create }}>
+			{children}
+		</CoursesContext.Provider>
+	);
+};
+
+export const useCoursesContext = () => useContext(CoursesContext);
