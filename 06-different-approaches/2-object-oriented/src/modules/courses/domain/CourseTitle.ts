735343b985dfcab7d28aae7672a3a6a720eee173
Nuria <soriano.nuria@gmail.com>
2023-04-05 17:53:56 +0200

    fix: add validation to CourseTitle in OO example

--- 06-different-approaches/2-object-oriented/src/modules/courses/domain/CourseTitle.ts
+++ 06-different-approaches/2-object-oriented/src/modules/courses/domain/CourseTitle.ts
@@ -1,3 +1,17 @@
 export class CourseTitle {
-	constructor(readonly value: string) {}
+	static readonly MAX_COURSE_LENGTH = 40;
+
+	constructor(readonly value: string) {
+		if (!CourseTitle.isValid(value)) {
+			throw new Error(CourseTitle.invalidMessage(value));
+		}
+	}
+
+	public static isValid(value: string): boolean {
+		return value.length > CourseTitle.MAX_COURSE_LENGTH;
+	}
+
+	public static invalidMessage(value: string): string {
+		return `The title [${value}] is too long. ${CourseTitle.MAX_COURSE_LENGTH} chars is the max allowed`;
+	}
 }
